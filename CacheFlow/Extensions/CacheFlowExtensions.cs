b9684acf6f83c6980e62aea4ee4d273a4802fef5
Kirill Taran <k.taran@doktornarabote.ru>
2018-06-09 15:53:22 +0300

    IServiceCollection extension has been added

---
+++ CacheFlow/Extensions/CacheFlowExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace CacheFlow.Extensions
+{
+    public static class CacheFlowExtensions
+    {
+        public static IServiceCollection UseCacheFlow(this IServiceCollection services)
+        {
+            services.AddSingleton<ICacheFlow, RedisFlow>();
+
+            return services;
+        }
+    }
+}
