13f9ed503d8d3980f5ffece18ba835c70b327d36
Kirill Taran <kirill.taran@hotmail.com>
2019-02-02 16:43:29 +0300

    Documentation

--- CacheFlow/FlowOptions.cs
+++ CacheFlow/FlowOptions.cs
@@ -7,6 +7,9 @@ namespace FloxDc.CacheFlow
     /// </summary>
     public class FlowOptions
     {
+        /// <summary>
+        /// The ratio between caching times in a memory and in a distributed cache when DoubleFlow is used.
+        /// </summary>
         public double DistributedToMemoryExpirationRatio { get; set; } = 10.0;
         /// <summary>
         /// Cache will be skipped for that time span if any error occurs in the request process.
