75853bd527ec4b0a9a80dcbbf4e741979e144c30
Kirill Taran <kirill.taran@hotmail.com>
2020-06-22 18:55:23 +0300

    Grammar improvements

--- CacheFlow/IDistributedFlow.cs
+++ CacheFlow/IDistributedFlow.cs
@@ -13,64 +13,64 @@ namespace FloxDc.CacheFlow
         FlowOptions Options { get; }
 
         /// <summary>
-        /// Gets value from cache.
+        /// Gets a value from a cache.
         /// </summary>
-        /// <typeparam name="T">Type of extracted value.</typeparam>
+        /// <typeparam name="T">Type of an extracted value.</typeparam>
         /// <param name="key">Cache key.</param>
         /// <param name="cancellationToken"></param>
         /// <returns></returns>
         Task<T> GetAsync<T>(string key, CancellationToken cancellationToken = default);
 
         /// <summary>
-        /// Tries to get value from cache, and sets it if no entry was found.
+        /// Tries to get a value from a cache, and sets it if no entries were found.
         /// </summary>
-        /// <typeparam name="T">Type of getted and setted value.</typeparam>
+        /// <typeparam name="T">Type of a got and a set value.</typeparam>
         /// <param name="key">Target cache key.</param>
-        /// <param name="getValueFunction">Function what gets value to set to cache. Executes if provided key wasn't found.</param>
-        /// <param name="absoluteExpirationRelativeToNow">Absolute amount of time relative to now which should pass to expire cache.</param>
+        /// <param name="getValueFunction">Function what gets value to set it to a cache. Executes if a provided key wasn't found.</param>
+        /// <param name="absoluteExpirationRelativeToNow">Absolute amount of time relative to now which should pass to expire a cache.</param>
         /// <returns></returns>
         T GetOrSet<T>(string key, Func<T> getValueFunction, TimeSpan absoluteExpirationRelativeToNow);
         
         /// <summary>
-        /// Tries to get value from cache, and sets it if no entry was found.
+        /// Tries to get a value from a cache, and sets it if no entries were found.
         /// </summary>
-        /// <typeparam name="T">Type of getted and setted value.</typeparam>
+        /// <typeparam name="T">Type of a got and a set value.</typeparam>
         /// <param name="key">Target cache key.</param>
-        /// <param name="getValueFunction">Function what gets value to set to cache. Executes if provided key wasn't found.</param>
+        /// <param name="getValueFunction">Function what gets a value to set it to a cache. Executes if a provided key wasn't found.</param>
         /// <param name="options">Cache options.</param>
         /// <returns></returns>
         T GetOrSet<T>(string key, Func<T> getValueFunction, DistributedCacheEntryOptions options);
 
         /// <summary>
-        /// Tries to get value from cache, and sets it if no entry was found.
+        /// Tries to get a value from a cache, and sets it if no entries were found.
         /// </summary>
-        /// <typeparam name="T">Type of getted and setted value.</typeparam>
+        /// <typeparam name="T">Type of a got and a set value.</typeparam>
         /// <param name="key">Target cache key.</param>
-        /// <param name="getValueFunction">Function what gets value to set to cache. Executes if provided key wasn't found.</param>
-        /// <param name="absoluteExpirationRelativeToNow">Absolute amount of time relative to now which should pass to expire cache.</param>
+        /// <param name="getValueFunction">Function what gets a value to set it to a cache. Executes if a provided key wasn't found.</param>
+        /// <param name="absoluteExpirationRelativeToNow">Absolute amount of time relative to now which should pass to expire a cache.</param>
         /// <param name="cancellationToken"></param>
         /// <returns></returns>
         Task<T> GetOrSetAsync<T>(string key, Func<Task<T>> getValueFunction, TimeSpan absoluteExpirationRelativeToNow, CancellationToken cancellationToken = default);
 
         /// <summary>
-        /// Tries to get value from cache, and sets it if no entry was found.
+        /// Tries to get a value from a cache, and sets it if no entries were found.
         /// </summary>
-        /// <typeparam name="T">Type of getted and setted value.</typeparam>
+        /// <typeparam name="T">Type of a got and a set value.</typeparam>
         /// <param name="key">Target cache key.</param>
-        /// <param name="getValueFunction">Function what gets value to set to cache. Executes if provided key wasn't found.</param>
+        /// <param name="getValueFunction">Function what gets a value to set it to a cache. Executes if a provided key wasn't found.</param>
         /// <param name="options">Cache options.</param>
         /// <param name="cancellationToken"></param>
         /// <returns></returns>
         Task<T> GetOrSetAsync<T>(string key, Func<Task<T>> getValueFunction, DistributedCacheEntryOptions options, CancellationToken cancellationToken = default);
 
         /// <summary>
-        /// Refreshes specific cache entry.
+        /// Refreshes a specified cache entry.
         /// </summary>
         /// <param name="key">Target cache key.</param>
         void Refresh(string key);
 
         /// <summary>
-        /// Refreshes specific cache entry.
+        /// Refreshes a specified cache entry.
         /// </summary>
         /// <param name="key">Target cache key.</param>
         /// <param name="cancellationToken"></param>
@@ -78,13 +78,13 @@ namespace FloxDc.CacheFlow
         Task RefreshAsync(string key, CancellationToken cancellationToken = default);
 
         /// <summary>
-        /// Removes specific cache entry.
+        /// Removes a specified cache entry.
         /// </summary>
         /// <param name="key">Target cache key.</param>
         void Remove(string key);
 
         /// <summary>
-        /// Removes specific cache entry.
+        /// Removes a specified cache entry.
         /// </summary>
         /// <param name="key">Target cache key.</param>
         /// <param name="cancellationToken"></param>
@@ -92,38 +92,38 @@ namespace FloxDc.CacheFlow
         Task RemoveAsync(string key, CancellationToken cancellationToken = default);
 
         /// <summary>
-        /// Sets cache entry with provided value.
+        /// Sets a cache entry with a provided value.
         /// </summary>
-        /// <typeparam name="T">Type of setted value.</typeparam>
+        /// <typeparam name="T">Type of a set value.</typeparam>
         /// <param name="key">Target cache key.</param>
         /// <param name="value">Value of the cache entry.</param>
-        /// <param name="absoluteExpirationRelativeToNow">Absolute amount of time relative to now which should pass to expire cache.</param>
+        /// <param name="absoluteExpirationRelativeToNow">Absolute amount of time relative to now which should pass to expire a cache.</param>
         void Set<T>(string key, T value, TimeSpan absoluteExpirationRelativeToNow);
 
         /// <summary>
-        /// Sets cache entry with provided value.
+        /// Sets a cache entry with a provided value.
         /// </summary>
-        /// <typeparam name="T">Type of setted value.</typeparam>
+        /// <typeparam name="T">Type of a set value.</typeparam>
         /// <param name="key">Target cache key.</param>
         /// <param name="value">Value of the cache entry.</param>
         /// <param name="options">Cache options.</param>
         void Set<T>(string key, T value, DistributedCacheEntryOptions options);
 
         /// <summary>
-        /// Sets cache entry with provided value.
+        /// Sets a cache entry with a provided value.
         /// </summary>
-        /// <typeparam name="T">Type of setted value.</typeparam>
+        /// <typeparam name="T">Type of a set value.</typeparam>
         /// <param name="key">Target cache key.</param>
         /// <param name="value">Value of the cache entry.</param>
-        /// <param name="absoluteExpirationRelativeToNow">Absolute amount of time relative to now which should pass to expire cache.</param>
+        /// <param name="absoluteExpirationRelativeToNow">Absolute amount of time relative to now which should pass to expire a cache.</param>
         /// <param name="cancellationToken"></param>
         /// <returns></returns>
         Task SetAsync<T>(string key, T value, TimeSpan absoluteExpirationRelativeToNow, CancellationToken cancellationToken = default);
 
         /// <summary>
-        /// Sets cache entry with provided value.
+        /// Sets a cache entry with a provided value.
         /// </summary>
-        /// <typeparam name="T">Type of setted value.</typeparam>
+        /// <typeparam name="T">Type of a set value.</typeparam>
         /// <param name="key">Target cache key.</param>
         /// <param name="value">Value of the cache entry.</param>
         /// <param name="options">Cache options.</param>
@@ -132,9 +132,9 @@ namespace FloxDc.CacheFlow
         Task SetAsync<T>(string key, T value, DistributedCacheEntryOptions options, CancellationToken cancellationToken = default);
         
         /// <summary>
-        /// Tries to get value from cache.
+        /// Tries to get a value from a cache.
         /// </summary>
-        /// <typeparam name="T">Type of extracted value.</typeparam>
+        /// <typeparam name="T">Type of an extracted value.</typeparam>
         /// <param name="key">Cache key.</param>
         /// <param name="value">Extracted value.</param>
         /// <returns></returns>
--- CacheFlow/IMemoryFlow.cs
+++ CacheFlow/IMemoryFlow.cs
@@ -13,75 +13,75 @@ namespace FloxDc.CacheFlow
         FlowOptions Options { get; }
 
         /// <summary>
-        /// Tries to get value from cache, and sets it if no entry was found.
+        /// Tries to get a value from a cache, and sets it if no entries were found.
         /// </summary>
-        /// <typeparam name="T">Type of getted and setted value.</typeparam>
+        /// <typeparam name="T">Type of a got and a set value.</typeparam>
         /// <param name="key">Target cache key.</param>
-        /// <param name="getValueFunction">Function what gets value to set to cache. Executes if provided key wasn't found.</param>
-        /// <param name="absoluteExpirationRelativeToNow">Absolute amount of time relative to now which should pass to expire cache.</param>
+        /// <param name="getValueFunction">Function what gets a value to set to a cache. Executes if provided key wasn't found.</param>
+        /// <param name="absoluteExpirationRelativeToNow">Absolute amount of time relative to now which should pass to expire a cache.</param>
         /// <returns></returns>
         T GetOrSet<T>(string key, Func<T> getValueFunction, TimeSpan absoluteExpirationRelativeToNow);
 
         /// <summary>
-        /// Tries to get value from cache, and sets it if no entry was found.
+        /// Tries to get a value from a cache, and sets it if no entries were found.
         /// </summary>
-        /// <typeparam name="T">Type of getted and setted value.</typeparam>
+        /// <typeparam name="T">Type of a got and a set value.</typeparam>
         /// <param name="key">Target cache key.</param>
-        /// <param name="getValueFunction">Function what gets value to set to cache. Executes if provided key wasn't found.</param>
+        /// <param name="getValueFunction">Function what gets a value to set to a cache. Executes if provided key wasn't found.</param>
         /// <param name="options">Cache options.</param>
         /// <returns></returns>
         T GetOrSet<T>(string key, Func<T> getValueFunction, MemoryCacheEntryOptions options);
 
         /// <summary>
-        /// Tries to get value from cache, and sets it if no entry was found.
+        /// Tries to get a value from a cache, and sets it if no entries were found.
         /// </summary>
-        /// <typeparam name="T">Type of getted and setted value.</typeparam>
+        /// <typeparam name="T">Type of a got and a set value.</typeparam>
         /// <param name="key">Target cache key.</param>
-        /// <param name="getValueFunction">Function what gets value to set to cache. Executes if provided key wasn't found.</param>
-        /// <param name="absoluteExpirationRelativeToNow">Absolute amount of time relative to now which should pass to expire cache.</param>
+        /// <param name="getValueFunction">Function what gets a value to set to a cache. Executes if provided key wasn't found.</param>
+        /// <param name="absoluteExpirationRelativeToNow">Absolute amount of time relative to now which should pass to expire a cache.</param>
         /// <param name="cancellationToken"></param>
         /// <returns></returns>
         ValueTask<T> GetOrSetAsync<T>(string key, Func<Task<T>> getValueFunction, TimeSpan absoluteExpirationRelativeToNow, CancellationToken cancellationToken = default);
 
         /// <summary>
-        /// Tries to get value from cache, and sets it if no entry was found.
+        /// Tries to get a value from a cache, and sets it if no entries were found.
         /// </summary>
-        /// <typeparam name="T">Type of getted and setted value.</typeparam>
+        /// <typeparam name="T">Type of a got and a set value.</typeparam>
         /// <param name="key">Target cache key.</param>
-        /// <param name="getValueFunction">Function what gets value to set to cache. Executes if provided key wasn't found.</param>
+        /// <param name="getValueFunction">Function what gets a value to set to a cache. Executes if provided key wasn't found.</param>
         /// <param name="options">Cache options.</param>
         /// <param name="cancellationToken"></param>
         /// <returns></returns>
         ValueTask<T> GetOrSetAsync<T>(string key, Func<Task<T>> getValueFunction, MemoryCacheEntryOptions options, CancellationToken cancellationToken = default);
 
         /// <summary>
-        /// Removes specific cache entry.
+        /// Removes a specified cache entry.
         /// </summary>
         /// <param name="key">Target cache key.</param>
         void Remove(string key);
 
         /// <summary>
-        /// Sets cache entry with provided value.
+        /// Sets a cache entry with a provided value.
         /// </summary>
-        /// <typeparam name="T">Type of setted value.</typeparam>
+        /// <typeparam name="T">Type of a set value.</typeparam>
         /// <param name="key">Target cache key.</param>
         /// <param name="value">Value of the cache entry.</param>
-        /// <param name="absoluteExpirationRelativeToNow">Absolute amount of time relative to now which should pass to expire cache.</param>
+        /// <param name="absoluteExpirationRelativeToNow">Absolute amount of time relative to now which should pass to expire a cache.</param>
         void Set<T>(string key, T value, TimeSpan absoluteExpirationRelativeToNow);
 
         /// <summary>
-        /// Sets cache entry with provided value.
+        /// Sets a cache entry with a provided value.
         /// </summary>
-        /// <typeparam name="T">Type of setted value.</typeparam>
+        /// <typeparam name="T">Type of a set value.</typeparam>
         /// <param name="key">Target cache key.</param>
         /// <param name="value">Value of the cache entry.</param>
         /// <param name="options">Cache options.</param>
         void Set<T>(string key, T value, MemoryCacheEntryOptions options);
 
         /// <summary>
-        /// Tries to get value from cache.
+        /// Tries to get a value from a cache.
         /// </summary>
-        /// <typeparam name="T">Type of extracted value.</typeparam>
+        /// <typeparam name="T">Type of an extracted value.</typeparam>
         /// <param name="key">Cache key.</param>
         /// <param name="value">Extracted value.</param>
         /// <returns></returns>
