8d4620f3df813f6a086f010e3a7ec181e958c3b0
Kirill Taran <kirill.taran@hotmail.com>
2021-02-20 13:59:52 +0300

    Time span extensions added

---
+++ CacheFlow/Extensions/TimeSpanExtensions.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace FloxDc.CacheFlow.Extensions
+{
+    public static class TimeSpanExtensions
+    {
+        public static TimeSpan BeforeMinuteEnds() 
+            => GetNextFrame(1);
+
+        
+        public static TimeSpan BeforeTwoMinutesEnd() 
+            => GetNextFrame(2);
+
+        
+        public static TimeSpan BeforeFiveMinutesEnd() 
+            => GetNextFrame(5);
+
+        
+        public static TimeSpan BeforeTenMinutesEnd() 
+            => GetNextFrame(10);
+
+        
+        public static TimeSpan BeforeQuarterHourEnds() 
+            => GetNextFrame(15);
+
+        
+        public static TimeSpan BeforeHalfHourEnds() 
+            => GetNextFrame(30);
+
+
+        public static TimeSpan BeforeHourEnds()
+        {
+            var now = DateTime.UtcNow;
+            var nextHour = new DateTime(now.Year, now.Month, now.Day, now.Hour, 0, 0, DateTimeKind.Utc).AddHours(1);
+
+            return nextHour - now;
+        }
+
+
+        public static TimeSpan BeforeDayEnds()
+        {
+            var now = DateTime.UtcNow;
+            var nextDay = new DateTime(now.Year, now.Month, now.Day, 0, 0, 0, DateTimeKind.Utc).AddDays(1);
+
+            return nextDay - now;
+        }
+
+        
+        private static TimeSpan GetNextFrame(int frameInMinutes)
+        {
+            var now = DateTime.UtcNow;
+            var frame = new DateTime(now.Year, now.Month, now.Day, now.Hour, now.Minute, 0, DateTimeKind.Utc).AddMinutes(frameInMinutes);
+
+            return frame - now;
+        }
+    }
+}
