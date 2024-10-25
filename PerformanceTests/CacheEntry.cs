700705bc24611e8fe54c95cb7b68c906b392411d
Kirill Taran <kirill.taran@hotmail.com>
2024-10-25 17:09:06 +0400

    Add PerformanceTests project.

---
+++ PerformanceTests/CacheEntry.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Primitives;
+
+internal class CacheEntry : ICacheEntry
+{
+    public CacheEntry(object key, Action<object> removeCallback)
+    {
+        Key = key;
+        _removeCallback = removeCallback;
+    }
+
+    public object Key { get; }
+    public object? Value { get; set; }
+    public DateTimeOffset? AbsoluteExpiration { get; set; }
+    public TimeSpan? AbsoluteExpirationRelativeToNow { get; set; }
+    public TimeSpan? SlidingExpiration { get; set; }
+    public IList<IChangeToken> ExpirationTokens { get; } = [];
+    public IList<PostEvictionCallbackRegistration> PostEvictionCallbacks { get; } = [];
+    public CacheItemPriority Priority { get; set; } = CacheItemPriority.Normal;
+    public long? Size { get; set; }
+
+
+    public bool Expired 
+        => AbsoluteExpiration.HasValue && AbsoluteExpiration.Value <= DateTimeOffset.Now;
+
+
+    IList<IChangeToken> ICacheEntry.ExpirationTokens 
+        => throw new NotImplementedException();
+
+
+    public void Dispose()
+    {
+        if (!_disposed)
+        {
+            _removeCallback(Key);
+            _disposed = true;
+        }
+    }
+
+
+    private readonly Action<object> _removeCallback;
+    private bool _disposed;
+}
---
+++ PerformanceTests/MemoryFlowBenchmark.cs
@@ -0,0 +1,68 @@
+﻿using BenchmarkDotNet.Attributes;
+using FloxDc.CacheFlow;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.Extensions.Options;
+
+[MemoryDiagnoser]
+public class MemoryFlowBenchmark
+{
+    [GlobalSetup]
+    public void Setup()
+    {
+        var memoryCache = new MemoryCache();
+        var options = Options.Create(new FlowOptions
+        {
+            CacheKeyPrefix = "test",
+            CacheKeyDelimiter = ":",
+            DataLoggingLevel = DataLogLevel.Normal,
+            SuppressCacheExceptions = true
+        });
+
+        _memoryFlow = new MemoryFlow(memoryCache, new NullLogger<MemoryFlow>(), options);
+    }
+
+    [Benchmark]
+    public void GetOrSet()
+    {
+        _memoryFlow.GetOrSet(Key, () => Value, _cacheEntryOptions);
+    }
+
+
+    [Benchmark]
+    public async Task GetOrSetAsync()
+    {
+        await _memoryFlow.GetOrSetAsync(Key, () => Task.FromResult(Value), _cacheEntryOptions);
+    }
+
+
+    [Benchmark]
+    public void Remove()
+    {
+        _memoryFlow.Remove(Key);
+    }
+
+
+    [Benchmark]
+    public void Set()
+    {
+        _memoryFlow.Set(Key, Value, _cacheEntryOptions);
+    }
+
+
+    [Benchmark]
+    public void TryGetValue()
+    {
+        _memoryFlow.TryGetValue(Key, out string value);
+    }
+
+    
+    private const string Key = "testKey";
+    private const string Value = "testValue";
+    private MemoryCacheEntryOptions _cacheEntryOptions = new()
+    {
+        AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(5)
+    };
+    
+    private MemoryFlow _memoryFlow;
+}
